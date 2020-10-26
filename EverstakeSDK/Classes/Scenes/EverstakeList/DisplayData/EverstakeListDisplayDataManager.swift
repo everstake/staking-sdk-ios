ae5bccf789edd5e2b4146e0e57b322432982a893
akoziaruk <a.koziaruk@gmail.com>
2020-10-26 16:39:19 +0200

    Added dynamic section logic

--- EverstakeSDK/Classes/Scenes/EverstakeList/DisplayData/EverstakeListDisplayDataManager.swift
+++ EverstakeSDK/Classes/Scenes/EverstakeList/DisplayData/EverstakeListDisplayDataManager.swift
@@ -11,6 +11,11 @@ import Kingfisher
 
 class EverstakeListDisplayDataManager: NSObject, UITableViewDataSource, UITableViewDelegate {
     
+    private enum SectionType {
+        case staked
+        case readyToStake
+    }
+    
     weak var tableView: UITableView! {
         didSet {
             setupTableView()
@@ -23,37 +28,54 @@ class EverstakeListDisplayDataManager: NSObject, UITableViewDataSource, UITableV
         }
     }
     
+//MARK: Private
+    
+    private func sectionTypeFor(_ section: Int) -> SectionType {
+        // Show only ready to stake list
+        if viewModel.steakedList.isEmpty {
+            return .readyToStake
+        }
+        // Show staked & ready to stake list
+        if section == 0 {
+            return .staked
+        } else {
+            return .readyToStake
+        }
+    }
+    
+    private func itemsFor(_ section: Int) -> [EverstakeList.CoinModel] {
+        switch sectionTypeFor(section) {
+        case .staked:
+            return viewModel.steakedList
+        case .readyToStake:
+            return viewModel.readyToStakeList
+        }
+    }
+    
+    private func titleFor(_ section: Int) -> String {
+        switch sectionTypeFor(section) {
+        case .staked:
+            return "STEAKED"
+        case .readyToStake:
+            return "READY TO STAKE"
+        }
+    }
+        
+//MARK: UITableViewDataSource
+    
     func numberOfSections(in tableView: UITableView) -> Int {
-        
-        // TODO: Make dynamic
-        
-        return 2
-        
+        return viewModel.steakedList.isEmpty ? 1 : 2
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        // TODO: Make dynamic
-        
-        switch section {
-        case 0:
-            return viewModel.steakedList.count
-        default:
-            return viewModel.readyToStakeList.count
-        }
+        return itemsFor(section).count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: Constants.coinCell) as! ESCoinTableViewCell
-        
-        //TODO: User view model here
-        
-        // TODO: Refactor
-        let coin = indexPath.section == 0 ? viewModel.steakedList[indexPath.item] :
-                                            viewModel.readyToStakeList[indexPath.item]
-        
-        
+            
+        let coin = itemsFor(indexPath.section)[indexPath.row]
         cell.logoImageView.kf.setImage(with: coin.iconUrl)
         cell.titleLabel.text = coin.title
         cell.subTitleLabel.text = "\(coin.apr ?? 0)%"
@@ -62,17 +84,15 @@ class EverstakeListDisplayDataManager: NSObject, UITableViewDataSource, UITableV
         return cell
     }
     
+//MARK: UITableViewDelegate
+    
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 38
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let header = tableView.dequeueReusableCell(withIdentifier: Constants.readyToStakeHeader) as! ESReadyToStakeHeader
-        
-        // TODO: Make dynamic
-
-        header.titleLabel.text = section == 0 ? "STEAKED" : "READY TO STAKE"
-        
+        header.titleLabel.text = titleFor(section)
         return header.contentView
     }
     
