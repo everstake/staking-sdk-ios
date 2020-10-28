0a2f755f977136ff2fb19cf09f3a9365086b7ef6
Alex Koziaruk <a.koziaruk@noisyminer.com>
2020-10-28 15:50:01 +0200

    Added NetworkManager & Assets

---
+++ EverstakeSDK/Classes/ESNetworkManager.swift
@@ -0,0 +1,41 @@
+//
+//  ESNetworkManager.swift
+//  EverstakeSDK
+//
+//  Created by Alexander Koziaruk on 23.10.2020.
+//
+
+import Foundation
+
+class ESNetworkManager {
+    
+    enum HTTPMethod: String {
+        case get = "GET"
+        case put = "PUT"
+        case post = "POST"
+        case delete = "DELETE"
+    }
+    
+    private struct Constants {
+        static let baseURL = "https://wallet-sdk-static.herokuapp.com/"
+    }
+    
+    static func loadDataWith(path: String, httpMethod: HTTPMethod, successWith: @escaping (Data) -> Void) {
+        let url = URL(string: Constants.baseURL + path)!
+        var request = URLRequest(url: url)
+        request.httpMethod = httpMethod.rawValue
+        let task = URLSession.shared.dataTask(with: request, completionHandler: { data, response, error in
+
+                if error == nil, let data = data {
+                    DispatchQueue.main.async {
+                        successWith(data)
+                    }
+                } else {
+                    print(error ?? "ESNetworkManager: Unknown Error \n\(String(describing: response?.description))")
+                }
+                    
+            })
+        task.resume()
+    
+    }
+}
